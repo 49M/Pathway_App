ba60139828b4c4fb6d526870e77e3fc0ab36f9bb
wehttqm <matthewfalcone5@gmail.com>
2025-02-15 15:01:33 -0500

    Added command on home page

--- Pathway-Next/pathway-next/app/page.tsx
+++ Pathway-Next/pathway-next/app/page.tsx
@@ -1,27 +1,50 @@
+"use client"
 import React from 'react';
 import Link from 'next/link';
+import { Command, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem, CommandSeparator, CommandDialog } from '@/components/ui/command';
+import { Button } from '@/components/ui/button';
+import { Calculator, CalculatorIcon, Calendar, Search, Smile } from 'lucide-react';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
+import { DialogTitle } from '@/components/ui/dialog';
+import { useRouter } from 'next/navigation';
 
 const sections = [
-    {name: "Mathematical Sciences", href: '/mathemetical-sciences'},
-    {name: "Computer Science", href: '/computer-science'}, 
-    {name: "Life Science", href: '/life-science'}
+    {name: "Mathematical Sciences", href: '/section/mathemetical-sciences'},
+    {name: "Computer Science", href: '/section/computer-science'}, 
+    {name: "Life Science", href: '/section/life-science'}
 ]
 
-export default function Home() {
+export default function Page() {
+  const [open, setOpen] = React.useState(false)
+  const router = useRouter()
+
   return (
-    <div className='flex flex-col w-full items-center'>
-      <div className=' header mb-20 mt-20'>
-        <h1>Pathway Course Planner</h1>
-      </div>
-      <div className='button-container'>
-        {sections.map(slug => {
-            return (<div key={slug.href}>
-            <Link href={`/section/${slug.href}`}>
-          <button>{slug.name}</button>
-        </Link>
-            </div>)
-        })}
-      </div>
+    <>
+    <div className='flex w-full flex-col'> 
+        <div className='w-full flex justify-center font-bold primary mt-20 text-[50px]'>
+            Pathway Course Planner
+        </div>
+        <div className='w-full flex justify-center mt-20'>
+            <Button variant="outline" className='font-bold' onClick={() => {setOpen(true)}}><Search/> Search programs at U of T</Button>
+        </div>
     </div>
-  );
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <DialogTitle></DialogTitle>
+        <CommandInput placeholder="Search for a program" />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            {sections.map((section) => {
+                return (
+                    <CommandItem className='hover:cursor-pointer' key={section.href}>
+                        <Link href={section.href}>{section.name}</Link>
+                    </CommandItem>
+                )
+            })}
+          </CommandGroup>
+          <CommandSeparator />
+        </CommandList>
+      </CommandDialog>
+    </>
+  )
 }
--- Pathway-Next/pathway-next/app/section/[slug]/page.tsx
+++ Pathway-Next/pathway-next/app/section/[slug]/page.tsx
@@ -13,26 +13,35 @@ import {
   CommandShortcut,
 } from "@/components/ui/command"
 
-const SectionPage = (params: { params: Promise<{ slug: string }> }) => {
-  return (
-    <div className="relative w-full h-screen justify-center">
-      <Header title={`Discover The Realm of ${"asdf"}`} />
-      <div>
-        <Command className="max-w-[calc(100vw-300px)] mx-auto w-full">
-          <CommandInput placeholder="Type a command or search..." />
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading="Example Prompts">
-              <CommandItem>I want to learn Java</CommandItem>
-              <CommandItem>I'm interested in anything data</CommandItem>
-              <CommandItem>I want to master AI and Deep Learning</CommandItem>
-            </CommandGroup>
-          </CommandList>
-        </Command>
-        <CategoryBubble/>
-      </div>
-    </div>
-  );
-};
+function get_name(str: string) {
+    const words = str.split("-")
+    var new_str = ""
+    for (let i = 0; i < words.length; i++) {
+        new_str = new_str + (words[i][0]).toUpperCase() + String(words[i]).slice(1) + " ";
+    }
+    return new_str;
+}
 
-export default SectionPage;
+export default async function Page({
+    params,
+  }: {
+    params: Promise<{ slug: string }>
+  }) {
+    const slug = (await params).slug
+    const name = get_name(slug)
+    return (
+        <div className="relative w-full h-screen justify-center">
+          <Header title={`Discover The Realm of ${name}`} />
+          <div>
+            <Command className="max-w-[calc(100vw-300px)] mx-auto w-full">
+              <CommandInput placeholder="" />
+              <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading="Example Prompts">
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </div>
+        </div>
+      );
+  }
