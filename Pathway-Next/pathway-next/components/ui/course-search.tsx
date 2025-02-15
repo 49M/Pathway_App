fe0863069010602b5ba8fe63d9e897c55814922d
wehttqm <matthewfalcone5@gmail.com>
2025-02-15 15:42:55 -0500

    Getting ready for db call

--- Pathway-Next/pathway-next/components/ui/course-search.tsx
+++ Pathway-Next/pathway-next/components/ui/course-search.tsx
@@ -1,13 +1,25 @@
 "use client";
+import { useState } from "react";
 import { SearchBar } from "./search-bar"
+import axios from "axios"
 
-export const CourseSearch = () => {
+export const CourseSearch = ({path}: {path: string}) => {
+    const [disabled, setDisabled] = useState(true)
     const onClick= () => {
-        
+        async function set_courses() {
+            const data = await axios.post('http://localhost:5328/set_courses', {
+                body: {
+                    "program_type": path
+                }
+            })
+        }
+
     }
     return (
         <div className='px-[30%]'>
-            <SearchBar onClick={onClick}></SearchBar>
+            {disabled && <span className='px-4 outline-2'>Fetching courses from U of T, please wait... (Max 30 seconds)</span>}
+            <SearchBar onClick={onClick} disabled={disabled}></SearchBar>
+            
         </div>
     )
 }
--- Pathway-Next/pathway-next/components/ui/search-bar.tsx
+++ Pathway-Next/pathway-next/components/ui/search-bar.tsx
@@ -2,38 +2,37 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "@/components/ui/accordion"
-
-export function SearchBar({onClick}: {onClick: any}) {
-  return (
-    <div>
-    <div className="flex w-full items-center space-x-2 ">
-      <Input type="email" className='bg-primary-foreground'placeholder="Message Pathway" />
-      <Button type="submit" onClick={onClick}>Submit</Button>
-    </div>
-    <Accordion type="single" collapsible className="w-full mt-40 space-y-2">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you
-          prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-    </div>
-  )
+export function SearchBar({ onClick, disabled }: { onClick: any, disabled: boolean }) {
+    return (
+        <div>
+            <div className="flex w-full items-center space-x-2 ">
+                <Input disabled={disabled} type="email" className='bg-primary-foreground' placeholder="Message Pathway" />
+                <Button disabled={disabled} type="submit" onClick={onClick}>Submit</Button>
+            </div>
+            <Accordion type="single" collapsible className="w-full mt-40 space-y-2">
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                    <AccordionTrigger>Is it styled?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It comes with default styles that matches the other
+                        components&apos; aesthetic.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                    <AccordionTrigger>Is it animated?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It&apos;s animated by default, but you can disable it if you
+                        prefer.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        </div>
+    )
 }
 
 const examples = [
