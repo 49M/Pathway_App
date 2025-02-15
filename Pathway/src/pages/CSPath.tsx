82bfec657b5a68cbec511ae612f60bc32bddf51b
49M <michal_buczek@yahoo.com>
2025-02-15 00:11:25 -0500

    cs button

---
+++ Pathway/src/pages/CSPath.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CSPath() {
+    return (
+        <div className='header mb-20 mt-20'>
+            Dicover The Realm of Computer Science
+        </div>
+    )
+}
+
+export default CSPath
